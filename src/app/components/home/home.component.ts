4d03f9fc61774a46ffd93d6b02ff9066fede7735
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-23 23:22:29 -0700

    Added some icons

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,5 +17,21 @@ import { ContactComponent } from "../contact/contact.component";
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
     AOS.init({ duration: 900, once: true });
+    this.skills = [...this.skills, ...this.skills];
   }
+    skills: string[] = [
+    'assets/Images/angular.png',
+    'assets/Images/dotnet.png',
+    'assets/Images/sql.png',
+    'assets/Images/leetcode.png',
+    'assets/Images/github.png',
+    'assets/Images/azure.png',
+    'assets/Images/html.png',
+    'assets/Images/css.png',
+    'assets/Images/bootstrap.png',
+    'assets/Images/tailwind.png',
+    'assets/Images/js.png',
+    'assets/Images/ts.png',
+    'assets/Images/figma.png',
+  ];
 }
