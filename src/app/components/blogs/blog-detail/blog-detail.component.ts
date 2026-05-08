4cba49c8d1b5c15a96f08136fff7ed5a00171eda
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-08 12:26:20 +0500

    added new file

--- src/app/components/blogs/blog-detail/blog-detail.component.ts
+++ src/app/components/blogs/blog-detail/blog-detail.component.ts
@@ -1,5 +1,12 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 
@@ -14,10 +21,14 @@ import { MarkdownModule } from 'ngx-markdown';
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss']
 })
-export class BlogDetailComponent implements OnInit {
+export class BlogDetailComponent
+  implements OnInit, AfterViewInit {
 
   slug = '';
   title = '';
+
+  @ViewChild('markdownContainer')
+  markdownContainer!: ElementRef<HTMLDivElement>;
 
   constructor(
     private route: ActivatedRoute
@@ -30,7 +41,69 @@ export class BlogDetailComponent implements OnInit {
 
     this.title = this.slug
       .replace(/-/g, ' ')
-      .replace(/\b\w/g, char => char.toUpperCase());
+      .replace(/\b\w/g, c => c.toUpperCase());
+  }
+
+  ngAfterViewInit(): void {
+
+    setTimeout(() => {
+      this.initializeCopyButtons();
+    }, 300);
+  }
+
+  private initializeCopyButtons(): void {
+
+    if (!this.markdownContainer) return;
+
+    const preBlocks =
+      this.markdownContainer
+        .nativeElement
+        .querySelectorAll('pre');
+
+    preBlocks.forEach((pre: HTMLElement) => {
+
+      if (pre.querySelector('.copy-btn')) {
+        return;
+      }
+
+      pre.classList.add('code-block');
+
+      const button =
+        document.createElement('button');
+
+      button.className = 'copy-btn';
+      button.type = 'button';
+      button.innerText = 'Copy';
+
+      button.addEventListener('click', async () => {
+
+        const code =
+          pre.querySelector('code')?.innerText || '';
+
+        try {
+
+          await navigator
+            .clipboard
+            .writeText(code);
+
+          button.innerText = 'Copied';
+
+          setTimeout(() => {
+            button.innerText = 'Copy';
+          }, 2000);
+
+        } catch {
+
+          button.innerText = 'Failed';
+
+          setTimeout(() => {
+            button.innerText = 'Copy';
+          }, 2000);
+        }
+      });
+
+      pre.appendChild(button);
+    });
   }
 
   get markdownPath(): string {
