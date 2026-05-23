b022a518a1de5498e2b830d1b835848bf3d87ce3
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-23 20:34:08 +0500

    added portfoli othumbnail

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,25 @@
-import {
-  ChangeDetectionStrategy,
-  Component
-} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-import { AboutComponent } from '../about/about.component';
+import { AboutComponent } from "../about/about.component";
 import { SkillsComponent } from '../skills/skills.component';
 import { PublicProjectsComponent } from '../projects/projects.component';
 import { ServicesComponent } from '../services/services.component';
 import { TestimonialComponent } from '../testimonial/testimonial.component';
-import { ContactComponent } from '../contact/contact.component';
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    ServicesComponent,
-    AboutComponent,
-    SkillsComponent,
-    PublicProjectsComponent,
-    TestimonialComponent,
-    ContactComponent
-  ],
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html',  
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [CommonModule, RouterModule, ServicesComponent, AboutComponent, SkillsComponent, PublicProjectsComponent, TestimonialComponent, ContactComponent],
 })
-export class HomeComponent {
-
-  readonly skills: readonly string[] = [
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.skills = [...this.skills, ...this.skills];
+  }
+    skills: string[] = [
     'assets/icons/angular.avif',
     'assets/icons/dotnet.avif',
     'assets/icons/sql.avif',
@@ -47,13 +34,5 @@ export class HomeComponent {
     'assets/icons/ts.avif',
     'assets/icons/figma.avif',
   ];
-
-  readonly duplicatedSkills = [
-    ...this.skills,
-    ...this.skills
-  ];
-
-  trackBySkill(index: number, item: string): string {
-    return item;
-  }
 }
+ 
